7962ece6e4d551d6d82a9a51aae39a7cd1175a7c
kamal <kamal.isham@tinkerlust.com>
2024-01-07 20:47:39 +0700

    init project

---
+++ controllers/users.js
@@ -0,0 +1,16 @@
+const example = require('../models/users_example')
+
+const Users = {
+  getAll: async (req, res, next) => {
+    return res.status(200).json({
+      message: "Get all user",
+      Success: true,
+      code: 200,
+      data: example.twilight,
+    });
+  }
+}
+
+
+
+module.exports = Users
