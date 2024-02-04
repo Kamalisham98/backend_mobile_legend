712b61a5fadd235020d0ffc0a26650f8eb0379e1
kamal <kamal.isham@tinkerlust.com>
2024-02-04 18:08:56 +0700

    deleted unused files

--- routes/users.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const Users = require('../controllers/users');
-/* GET users listing. */
-router.get('/', Users.getAll);
-
-
-module.exports = router;
